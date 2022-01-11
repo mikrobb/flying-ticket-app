98d4f2bba6357123350fc021bcb45a41eba2f802
mikrob <promikrob@gmail.com>
2022-01-11 18:26:00 +0200

    finish styling and add show more tickets

--- src/store.js
+++ src/store.js
@@ -2,12 +2,20 @@ import { createStore } from "redux";
 
 const initialState = {
     tickets: null,
+    currentPage :5,
+    findChekbox: 'free'
 }
 
 function reducer(state = initialState , action){
 switch(action.type){
     case 'searchTickets':{
         return{...state, tickets: action.payload}
+    }
+    case 'SetCurrentPage':{
+        return{...state, currentPage : action.payload}
+    }
+    case 'findTicket':{
+        return{...state , findChekbox: action.payload}
     }
     default: {
         return state
